eee8344c3695543973ca52e3bc333b11c38fb0a2
louay17699 <chamrihamouda@gmail.com>
2025-06-16 19:23:30 +0100

    FLARY

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import wallpaperRoute from "./routes/wallpaperRoute.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 4000 
+const PORT = process.env.PORT || 4000 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
