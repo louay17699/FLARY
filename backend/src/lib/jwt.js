3ad7fb6cbdcc413ccb490401e98d8489df3e6bc3
louay17699 <chamrihamouda@gmail.com>
2025-06-16 19:30:38 +0100

    FLARY

--- backend/src/lib/jwt.js
+++ backend/src/lib/jwt.js
@@ -8,7 +8,7 @@ export const generateToken =(userId,res)=>{
 
 res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
-  
+  httpOnly: true,
   sameSite: "none",  
   secure: true       
 });
