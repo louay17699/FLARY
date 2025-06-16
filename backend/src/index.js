5b37dc00e443f76c3b68db834a6120aef9b47e5e
louay17699 <chamrihamouda@gmail.com>
2025-06-16 19:01:44 +0100

    FLARY

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://flary-frontend.onrender.com",
     credentials: true,
 }));
 
