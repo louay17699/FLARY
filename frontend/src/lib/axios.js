4cfa14e13b8dadc45bb20ef034525665e0bb25a7
louay17699 <chamrihamouda@gmail.com>
2025-06-16 22:04:09 +0100

    FLARY

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://flary.onrender.com/api",
-    withCredentials: true,
-})
+  baseURL: "https://flary.onrender.com/api", // Must match your backend
+  withCredentials: true, // Crucial for sending cookies
+});
